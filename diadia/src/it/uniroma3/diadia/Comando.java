fdef5b6508d3cca1c1adf4567fc25388ecd1c08d
Stantuffy <tontonialberto@outlook.it>
2019-04-03 15:56:12 +0200

    Aggiunta la factory dei comandi, l'interfaccia Comando e le classi per ogni comando.

--- diadia/src/it/uniroma3/diadia/Comando.java
+++ diadia/src/it/uniroma3/diadia/Comando.java
@@ -1,49 +1,7 @@
 package it.uniroma3.diadia;
 
-
-import java.util.Scanner;
-
-/**
- * Questa classe modella un comando.
- * Un comando consiste al piu' di due parole:
- * il nome del comando ed un parametro
- * su cui si applica il comando.
- * (Ad es. alla riga digitata dall'utente "vai nord"
- *  corrisponde un comando di nome "vai" e parametro "nord").
- *
- * @author  docente di POO
- * @version base
- */
-
-public class Comando {
-
-    private String nome;
-    private String parametro;
-
-    public Comando(String istruzione) {
-    	this.nome = "";
-    	this.parametro = "";
-  
-		Scanner scannerDiParole = new Scanner(istruzione);
-
-		// prima parola: nome del comando
-		if (scannerDiParole.hasNext())
-			this.nome = scannerDiParole.next(); 
-
-		// seconda parola: eventuale parametro
-		if (scannerDiParole.hasNext())
-			this.parametro = scannerDiParole.next();
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public String getParametro() {
-        return this.parametro;
-    }
-
-    public boolean sconosciuto() {
-        return (this.nome == null);
-    }
+public interface Comando {
+	public void setParametro(String parametro);
+	
+	public void esegui(Partita partita);
 }
--- diadia/src/it/uniroma3/diadia/DiaDia.java
+++ diadia/src/it/uniroma3/diadia/DiaDia.java
@@ -29,8 +29,6 @@ public class DiaDia {
 			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
 			"Per conoscere le istruzioni usa il comando 'aiuto'.";
 	
-	static final private String[] elencoComandi = {"vai", "prendi", "posa", "aiuto", "borsa", "status", "fine"};
-
 	private Partita partita;
 
 	public DiaDia() {
@@ -55,25 +53,10 @@ public class DiaDia {
 	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
 	 */
 	private boolean processaIstruzione(String istruzione) {
-		Comando comandoDaEseguire = new Comando(istruzione);
+		FabbricaDiComandi factory = new FabbricaDiComandi();
+		Comando comando = factory.creaComando(istruzione);
 		
-		if (comandoDaEseguire.getNome().equals("fine")) {
-			this.fine(); 
-			return true;
-		} else if (comandoDaEseguire.getNome().equals("vai"))
-			this.vai(comandoDaEseguire.getParametro());
-		else if (comandoDaEseguire.getNome().equals("aiuto"))
-			this.aiuto();
-		else if(comandoDaEseguire.getNome().equals("prendi"))
-			this.prendi(comandoDaEseguire.getParametro());
-		else if(comandoDaEseguire.getNome().equals("posa"))
-			this.posa(comandoDaEseguire.getParametro());
-		else if(comandoDaEseguire.getNome().equals("borsa"))
-			System.out.println(this.partita.getGiocatore().getBorsa());
-		else if(comandoDaEseguire.getNome().equals("status"))
-			System.out.println(this.partita.getGiocatore());
-		else
-			System.out.println("Comando sconosciuto");
+		comando.esegui(this.partita);
 		
 		if (this.partita.vinta()) {
 			System.out.println("Hai vinto!");
@@ -83,98 +66,6 @@ public class DiaDia {
 			return true;
 		}		
 		return false;
-	}   
-
-	// implementazioni dei comandi dell'utente:
-
-	/**
-	 * Stampa informazioni di aiuto.
-	 */
-	private void aiuto() {
-		for(int i=0; i< elencoComandi.length; i++) 
-			System.out.print(elencoComandi[i]+" ");
-		System.out.println();
-	}
-
-	/**
-	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra 
-	 * e ne stampa il nome, altrimenti stampa un messaggio di errore
-	 */
-	private void vai(String direzione) {
-		if(direzione==null)
-			System.out.println("Dove vuoi andare ?");
-		Stanza prossimaStanza = null;
-		prossimaStanza = this.partita.getStanzaCorrente().getStanzaAdiacente(direzione);
-		if (prossimaStanza == null)
-			System.out.println("Direzione inesistente");
-		else {
-			this.partita.setStanzaCorrente(prossimaStanza);
-			int cfu = this.partita.getGiocatore().getCfu();
-			this.partita.getGiocatore().setCfu(cfu - 5);
-		}
-		System.out.println(partita.getStanzaCorrente().getDescrizione());
-	}
-
-	/**
-	 * Comando "Fine".
-	 */
-	private void fine() {
-		System.out.println("Grazie di aver giocato!");  // si desidera smettere
-	}
-	
-	/**
-	 * Comando "prendi". Il giocatore prende un attrezzo nella
-	 * stanza  e lo mette in borsa.
-	 * 
-	 * @param nomeAttrezzo
-	 */
-	private void prendi(String nomeAttrezzo) {
-		// Controllo se l'attrezzo esiste nella stanza
-		if(! this.partita.getStanzaCorrente().hasAttrezzo(nomeAttrezzo)) {
-			System.out.println("Non puoi prendere l'oggetto " + nomeAttrezzo + ".");
-			return;
-		}
-		
-		// Controllo se il giocatore ha spazio nella borsa
-		Attrezzo attrezzo = this.partita.getStanzaCorrente().getAttrezzo(nomeAttrezzo);
-		if(! this.partita.getGiocatore().prendi(attrezzo)) {
-			System.out.println("Non hai più spazio nella borsa.");
-			return;
-		}
-		
-		if(! this.partita.getStanzaCorrente().removeAttrezzo(attrezzo.getNome())) {
-			System.out.println("Impossibile raccogliere l'oggetto");
-			return;
-		}
-		
-		// Rimuovo l'attrezzo dalla stanza
-		System.out.println(nomeAttrezzo + " aggiunto alla borsa.");
-	}
-	
-	/**
-	 * Comando "posa". Un attrezzo viene rimosso dalla borsa del giocatore
-	 * e viene posato nella stanza corrente.
-	 * 
-	 * @param nomeAttrezzo
-	 */
-	private void posa(String nomeAttrezzo) {
-		// Controllo se tale attrezzo esiste nella borsa
-		Attrezzo attrezzo = this.partita.getGiocatore().posa(nomeAttrezzo);
-		if(attrezzo == null) {
-			System.out.println("Non possiedi un oggetto con quel nome.");
-			return;
-		}
-		
-		// Controllo se nella stanza c'è spazio a sufficienza.
-		if(! this.partita.getStanzaCorrente().addAttrezzo(attrezzo)) {
-			System.out.println("Non c'è abbastanza spazio nella stanza.");
-			
-			// Ripristino l'attrezzo nella borsa del giocatore.
-			this.partita.getGiocatore().prendi(attrezzo);
-			return;
-		}
-		
-		System.out.println("Hai lasciato cadere " + nomeAttrezzo);
 	}
 
 	public static void main(String[] argc) {
